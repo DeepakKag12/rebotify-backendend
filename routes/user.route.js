9ef19c9ff1e664949ee23b32689e83c215fd8dfe
Deepak Kag <kagdeepak45@gmail.com>
2025-12-12 15:14:08 +0530

    feat: Add login method options - password or OTP-based authentication

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 import {
   signup,
   login,
+  loginWithOTP,
   verifyOTP,
   getAllUsers,
   getUserById,
@@ -19,6 +20,7 @@ const router = express.Router();
 // Public routes
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/login-with-otp", loginWithOTP);
 router.post("/verify-otp", verifyOTP);
 router.post("/logout", logout);
 
